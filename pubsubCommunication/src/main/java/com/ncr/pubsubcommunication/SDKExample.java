3f7712ba9186f7f4c6612a1ab239e079b681e69b
whgiles <hg185078@ncr.com>
2022-08-05 10:50:31 -0400

    starting remote repo

---
+++ pubsubCommunication/src/main/java/com/ncr/pubsubcommunication/SDKExample.java
@@ -0,0 +1,13 @@
+package com.ncr.pubsubcommunication;
+
+public class SDKExample {
+    private final int num;
+
+    public SDKExample(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
